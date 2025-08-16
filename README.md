4fea66067380cc08d8dc1544eef613133edbbaea
Sampat Kumar <sampt.kumar@clicflyer.com>
2025-08-16 16:27:48 +0530

    unit test cases updated

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # CICD-Pipeline
-This repo is about, how can we implement CICD Pipeline 
+This repo is about, how can we implement CICD Pipeline
---
+++ tests/test_operations.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from scr.math_operations import add, subtract
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(-1, -1) == 0
+    assert subtract(0, 0) == 0
